44bee0a15214f5ded581aaee0fca34ca9218afe7
Brooklss <>
2023-03-28 12:22:03 -0700

    Custom function for _printf to handle binary nums

---
+++ handle_binary.c
@@ -0,0 +1,78 @@
+#include "main.h"
+
+/**
+ * handle_binary - prints decimal in binary
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int handle_binary(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input, count, i, first_one, isnegative;
+	char *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = check_buff(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	first_one = 0;
+	for (count = i = 0; binary[i]; i++)
+	{
+		if (first_one == 0 && binary[i] == '1')
+			first_one = 1;
+		if (first_one == 1)
+		{
+			ibuf = check_buff(buf, binary[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	return (count);
+}
+
+/**
+ * fill_binary_array - prints decimal in binary
+ * @binary: pointer to binary
+ * @int_in: input number
+ * @isneg: if input number is negative
+ * @limit: size of the binary
+ * Return: number of chars printed.
+ */
+char *fill_binary_array(char *binary, long int int_in, int isneg, int limit)
+{
+	int i;
+
+	for (i = 0; i < limit; i++)
+		binary[i] = '0';
+	binary[limit] = '\0';
+	for (i = limit - 1; int_in > 1; i--)
+	{
+		if (int_in == 2)
+			binary[i] = '0';
+		else
+			binary[i] = (int_in % 2) + '0';
+		int_in /= 2;
+	}
+	if (int_in != 0)
+		binary[i] = '1';
+	if (isneg)
+	{
+		for (i = 0; binary[i]; i++)
+			if (binary[i] == '0')
+				binary[i] = '1';
+			else
+				binary[i] = '0';
+	}
+	return (binary);
+}
