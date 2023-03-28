e7aabbca4219a727018b65a90450a9a372709d13
Brooklss <>
2023-03-28 13:34:35 -0700

    Helper function for _printf to handle octal numbers

---
+++ handle_oct.c
@@ -0,0 +1,117 @@
+#include "main.h"
+
+/**
+ * handle_oct - prints decimal number in octal
+ * @arguments: input number
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed.
+ */
+int handle_oct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = check_buff(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = check_buff(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count);
+}
+
+
+/**
+ * handle_noct - print the number in octal begining with zero
+ * @arguments: input string
+ * @buf: buffer pointer
+ * @ibuf: index for buffer pointer
+ * Return: number of chars printed
+ */
+int handle_noct(va_list arguments, char *buf, unsigned int ibuf)
+{
+	int int_input, i, isnegative, count, first_digit;
+	char *octal, *binary;
+
+	int_input = va_arg(arguments, int);
+	isnegative = 0;
+	if (int_input == 0)
+	{
+		ibuf = check_buff(buf, '0', ibuf);
+		return (1);
+	}
+	if (int_input < 0)
+	{
+		int_input = (int_input * -1) - 1;
+		isnegative = 1;
+	}
+	ibuf = handl_buf(buf, '0', ibuf);
+	binary = malloc(sizeof(char) * (32 + 1));
+	binary = fill_binary_array(binary, int_input, isnegative, 32);
+	octal = malloc(sizeof(char) * (11 + 1));
+	octal = fill_oct_array(binary, octal);
+	for (first_digit = i = count = 0; octal[i]; i++)
+	{
+		if (octal[i] != '0' && first_digit == 0)
+			first_digit = 1;
+		if (first_digit)
+		{
+			ibuf = check_buff(buf, octal[i], ibuf);
+			count++;
+		}
+	}
+	free(binary);
+	free(octal);
+	return (count + 1);
+}
+
+
+/**
+ * fill_oct_array - writes the character c to stdout
+ *
+ * @bnr: array where is stored the binary.
+ * @oct: array where is stored the octal.
+ *
+ * Return: binary array.
+ */
+char *fill_oct_array(char *bnr, char *oct)
+{
+	int op, i, j, ioct, limit;
+
+	oct[11] = '\0';
+	for (i = 31, ioct = 10; i >= 0; i--, ioct--)
+	{
+		if (i > 1)
+			limit = 4;
+		else
+			limit = 2;
+		for (op = 0, j = 1; j <= limit; j *= 2, i--)
+			op = ((bnr[i] - '0') * j) + op;
+		i++;
+		oct[ioct] = op + '0';
+	}
+	return (oct);
+}
